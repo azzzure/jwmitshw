a9b5f77bafa42da17e08b041a751492f6f870b40
Wang Ran <16307110315@fudan.edu.cn>
2022-05-24 16:54:01 -0400

    支持任意长度文本,修改了UI,删除了显示私钥的功能

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # jwmitshw
 一个简单的加密通话软件
+
+gui程序的框架来源于
+https://www.runoob.com/python/python-gui-tkinter.html
--- client.py
+++ client.py
@@ -47,14 +47,34 @@ class MY_RSA():
         self.rpk=rsa.PublicKey.load_pkcs1(rpk)
     def encryp(self,msg):
         msg_bytes=bytes(msg,"utf-8")
-        msg_enc=rsa.encrypt(msg_bytes,self.rpk)
-        msg_enc_b64=base64.b64encode(msg_enc)
+        # cut it into smaller fragment
+        # L= k*110+r
+        L=len(msg_bytes)
+        k=L//110
+        r=L%110
+        msg_enc_b64=b''
+        for i in range(k):
+            msg_enc=rsa.encrypt(msg_bytes[110*i:110*i+110],self.rpk)
+            msg_enc_b64+=base64.b64encode(msg_enc)+b'|'
+
+        if r!=0:
+            msg_enc=rsa.encrypt(msg_bytes[L-r:],self.rpk)
+            msg_enc_b64+=base64.b64encode(msg_enc)
+        else:
+            msg_enc_b64=msg_enc_b64[:-1]
         # print(msg_enc_b64)
         return msg_enc_b64
     def decrypt(self,msg_enc_b64):
-        msg_enc=base64.b64decode(msg_enc_b64)
-        print(msg_enc)
-        msg_bytes=rsa.decrypt(msg_enc,self.sk)
+        msg_enc_b64=msg_enc_b64.split('|')
+        msg=''
+        msg_bytes=b''
+        for msg in msg_enc_b64:
+            msg=base64.b64decode(msg)
+            # print(msg)
+            msg_bytes+=rsa.decrypt(msg,self.sk)
+            temp=rsa.decrypt(msg,self.sk)
+            # print(temp,end='\n\n')
+        # print(msg_bytes)
         msg=str(msg_bytes,"utf-8")
         # print(msg)
         return msg
@@ -71,13 +91,10 @@ class MY_GUI():
     # 设置窗口
 
     def set_init_window(self):
-        self.init_window_name.title("从runnoob上偷来的文本处理工具_v1.2")  # 窗口名
-        # self.init_window_name.geometry('320x160+10+10')                         #290 160为窗口大小，+10 +10 定义窗口弹出时的默认展示位置
+        self.init_window_name.title("加密通话")  # 窗口名
         self.init_window_name.geometry('1068x681+10+10')
-        # self.init_window_name["bg"] = "pink"                                    #窗口背景色，其他背景色见：blog.csdn.net/chl0000/article/details/7657887
-        # self.init_window_name.attributes("-alpha",0.9)                          #虚化，值越小虚化程度越高
-
         # 标签
+
         self.init_data_label = Label(self.init_window_name, text="你的公钥")
         self.init_data_label.grid(row=0, column=0)
 
@@ -93,14 +110,17 @@ class MY_GUI():
         self.msg2_label = Label(self.init_window_name, text="处理后文本")
         self.msg2_label.grid(row=21, column=10)
 
+        self.log_label = Label(self.init_window_name, text="日志")
+        self.log_label.grid(row=32, column=0)
+
         # 文本框
         # 你的公钥
         self.pk_Text = Text(self.init_window_name, width=65, height=6)
         self.pk_Text.grid(row=1, column=0, rowspan=6, columnspan=10)
         # 显示私钥
 
-        self.sk_Text = Text(self.init_window_name, show='*',width=65, height=15)
-        self.sk_Text.grid(row=8, column=0, rowspan=15, columnspan=10)
+        self.sk_Text = Text(self.init_window_name, width=65, height=3)
+        self.sk_Text.grid(row=8, column=0, rowspan=3, columnspan=10)
         # 接受的公钥
         self.rpk_Text = Text(self.init_window_name, width=65, height=6)
         self.rpk_Text.grid(row=24, column=0, columnspan=10)
@@ -111,15 +131,17 @@ class MY_GUI():
         # 处理后文本
         self.msg2_Text = Text(self.init_window_name, width=65, height=20)
         self.msg2_Text.grid(row=22, column=10, columnspan=10, rowspan=20)
-
+        # 日志
+        self.log_Text = Text(self.init_window_name, width=65, height=20)
+        self.log_Text.grid(row=33, column=0, columnspan=6, rowspan=20)
         # 按钮
         self.genratekey = Button(self.init_window_name, text="生成公私钥",
                                  bg="lightblue", width=10, command=self.gen_keypair)
-        self.genratekey.grid(row=31, column=0)
+        self.genratekey.grid(row=20, column=0)
 
         self.loadkeypair = Button(self.init_window_name, text="加载公私钥",
                                  bg="lightblue", width=10, command=self.load_keypair)
-        self.loadkeypair.grid(row=31, column=1)
+        self.loadkeypair.grid(row=20, column=1)
 
         self.process = Button(self.init_window_name, text="处理!",
                                  bg="lightblue", width=16,height=2, command=self.process_text)
@@ -129,10 +151,13 @@ class MY_GUI():
                                  bg="lightblue", width=16,height=2, command=self.accpet_pk)
         self.accpetpk.grid(row=23, column=1)
 
+        #INIT
+        self.write_log_to_Text("启动成功")
 
     # 功能函数
     def accpet_pk(self):
         rpk = self.rpk_Text.get(1.0,END)
+        self.write_log_to_Text("接受公钥")
         # print(rpk)
 
         self.rsa.loadrpk(rpk)
@@ -141,7 +166,8 @@ class MY_GUI():
         self.pk_Text.delete(1.0, END)
         self.pk_Text.insert(1.0, self.rsa.pkbyte())
         self.sk_Text.delete(1.0, END)
-        self.sk_Text.insert(1.0, self.rsa.skbyte())
+        self.sk_Text.insert(1.0, "任何情况下,你都不应该公开你的私钥")
+        self.write_log_to_Text("加载秘钥对")
         pass
     def gen_keypair(self):
         self.rsa.genkeypair()
@@ -149,6 +175,7 @@ class MY_GUI():
         self.pk_Text.insert(1.0, self.rsa.pkbyte())
         self.sk_Text.delete(1.0, END)
         self.sk_Text.insert(1.0, self.rsa.skbyte())
+        self.write_log_to_Text("生成秘钥对")
     def copypkto_clipboard(self):
         # pyperclip.copy("haha")
         pass
@@ -160,57 +187,21 @@ class MY_GUI():
         self.rpk_Text.insert(1.0, rpk)
     def process_text(self):
         src = self.msg1_Text.get(1.0,END)
-        print(src)
+        # print(src)
         if src:
             try:
                 msg=self.rsa.decrypt(src)
                 self.msg2_Text.delete(1.0, END)
                 self.msg2_Text.insert(1.0, msg)
+                self.write_log_to_Text("解密信息")
             except:
                 msg=self.rsa.encryp(src)
                 self.msg2_Text.delete(1.0, END)
                 self.msg2_Text.insert(1.0, msg)
+                self.write_log_to_Text("加密信息")
 
+    
 
-        pass
-    def str_trans_to_md5(self):
-        src = self.init_data_Text.get(
-            1.0, END).strip().replace("\n", "").encode()
-        #print("src =",src)
-        if src:
-            try:
-                myMd5 = hashlib.md5()
-                myMd5.update(src)
-                myMd5_Digest = myMd5.hexdigest()
-                # print(myMd5_Digest)
-                # 输出到界面
-                self.result_data_Text.delete(1.0, END)
-                self.result_data_Text.insert(1.0, myMd5_Digest)
-                self.write_log_to_Text("INFO:str_trans_to_md5 success")
-            except:
-                self.result_data_Text.delete(1.0, END)
-                self.result_data_Text.insert(1.0, "字符串转MD5失败")
-        else:
-            self.write_log_to_Text("ERROR:str_trans_to_md5 failed")
-
-    def decrypt(self):
-        src = self.init_data_Text.get(
-            1.0, END).strip().replace("\n", "").encode()
-        if src:
-            try:
-                myMd5 = hashlib.md5()
-                myMd5.update(src)
-                myMd5_Digest = b"asdf"
-                # print(myMd5_Digest)
-                # 输出到界面
-                self.result_data_Text.delete(1.0, END)
-                self.result_data_Text.insert(1.0, myMd5_Digest)
-                self.write_log_to_Text("INFO:str_trans_to_md5 success")
-            except:
-                self.result_data_Text.delete(1.0, END)
-                self.result_data_Text.insert(1.0, "字符串转MD5失败")
-        else:
-            self.write_log_to_Text("ERROR:str_trans_to_md5 failed")
     # 获取当前时间
 
     def get_current_time(self):
@@ -224,11 +215,11 @@ class MY_GUI():
         current_time = self.get_current_time()
         logmsg_in = str(current_time) + " " + str(logmsg) + "\n"  # 换行
         if LOG_LINE_NUM <= 7:
-            self.log_data_Text.insert(END, logmsg_in)
+            self.log_Text.insert(END, logmsg_in)
             LOG_LINE_NUM = LOG_LINE_NUM + 1
         else:
-            self.log_data_Text.delete(1.0, 2.0)
-            self.log_data_Text.insert(END, logmsg_in)
+            self.log_Text.delete(1.0, 2.0)
+            self.log_Text.insert(END, logmsg_in)
 
 
 def gui_start():
